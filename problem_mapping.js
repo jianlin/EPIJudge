f1d0f369ea1f404c30a79767bd5d6c653bcc91da
Kin <winterheat@gmail.com>
2020-03-04 14:48:31 -0800

    ok

--- problem_mapping.js
+++ problem_mapping.js
@@ -10,7 +10,7 @@ problem_mapping = {
                 "total": 10001
             },
             "Python: count_bits.py": {
-                "passed": 10001,
+                "passed": 0,
                 "total": 10001
             }
         },
