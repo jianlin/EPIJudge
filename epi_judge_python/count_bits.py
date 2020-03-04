bd5b85b01cb4fd1998834b5ddf72209830f30f6d
Kin <winterheat@gmail.com>
2020-03-04 14:21:52 -0800

    ok

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -1,14 +1,23 @@
 from test_framework import generic_test
 
 
+# def count_bits(x: int) -> int:
+#     # TODO - you fill in here.
+#     c = 0
+#     while x:
+#         x = x & (x - 1)
+#         c += 1
+#     # return c 
+#     return 0
+
+
 def count_bits(x: int) -> int:
-    # TODO - you fill in here.
-    c = 0
+
+    num_bits = 0
     while x:
-        x = x & (x - 1)
-        c += 1
-    # return c 
-    return 0
+        num_bits += x & 1
+        x >>= 1
+    return num_bits
 
 
 if __name__ == '__main__':
