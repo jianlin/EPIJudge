0450e1407104ef1d950d5f218d038df9a53575fb
Kin <winterheat@gmail.com>
2020-03-05 03:11:11 -0800

    ok

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,12 @@ from test_framework import generic_test
 
 
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    p = 0
+    while x:
+        p = ~p
+        x = x & (x-1)
+    p = 0
+    return p & 1
 
 
 if __name__ == '__main__':
