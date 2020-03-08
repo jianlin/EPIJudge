4cccf8d522ef727c96ebd47320aae0ab8cba439a
Kin <winterheat@gmail.com>
2020-03-08 06:38:18 -0700

    ok

--- epi_judge_python_solutions/parity.answer.py
+++ epi_judge_python_solutions/parity.answer.py
@@ -5,12 +5,7 @@ def parity(x: int) -> int:
     while x:
         p = ~p
         x = x & (x-1)
-<<<<<<< HEAD
     return p & 1 
-=======
-    p = 0
-    return p & 1
->>>>>>> d1a03d19726d39edcf9b708ef0336114ed5322f1
 
 
 if __name__ == '__main__':
